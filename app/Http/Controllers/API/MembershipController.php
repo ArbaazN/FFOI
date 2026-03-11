53ecfdd9fbfc446727c181bb4a1e7467b705acc6
jyoti2320 <68075592+jyoti2320@users.noreply.github.com>
2026-03-11 12:56:03 +0530

    done

---
+++ app/Http/Controllers/API/MembershipController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\MemberShip;
+use App\Models\MembershipBenefit;
+use App\Models\MembershipType;
+
+use Illuminate\Support\Facades\Log;
+
+class MembershipController extends Controller
+{
+    public function membershipDetail()
+    {
+        try {
+
+           $membership = MemberShip::get();
+            $features   = MembershipBenefit::get();
+            $plans      = MembershipType::get();
+
+            return response()->json([
+                'status' => true,
+                'data'   => [
+                    'membership' => $membership,
+                    'Benefit'   => $features,
+                    'Type'      => $plans,
+                ],
+            ], 200);
+
+        } catch (\Throwable $e) {
+
+            Log::error('Membership API Error', [
+                'error' => $e->getMessage(),
+                'line'  => $e->getLine(),
+                'file'  => $e->getFile(),
+            ]);
+
+            return response()->json([
+                'status'  => false,
+                'message' => 'Something went wrong while fetching membership data.',
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\API\UtmApiController;
 use App\Http\Controllers\API\WebsiteApiController;
 use App\Http\Controllers\API\ContactController;
 use App\Http\Controllers\API\WebinarController;
+use App\Http\Controllers\API\MembershipController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ Route::middleware('api_key')->group(function () {
     Route::get('/webinar', [WebinarController::class, 'show']);
     Route::get('/sessions/{slug}', [WebinarController::class, 'sessionDetail']);
     Route::POST('/webinar/registration', [WebinarController::class, 'saveWebinar']);
+
+    Route::get('/membership', [MembershipController::class, 'membershipDetail']);
 
     Route::get('/allBlogs', [BlogApiController::class, 'latestBlogs']);
     Route::get('/blog/{slug}', [BlogApiController::class, 'blogDetail']);
